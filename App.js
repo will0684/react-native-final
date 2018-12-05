336958f1eda25ff93c3731c45053d4d2dce3a238
will0684 <jordan.walcaraz@gmail.com>
2018-12-05 15:18:00 -0500

    Added react-navigation and added some components

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Container, Header, Content, Button, Text, Spinner, Title, Body } from "native-base";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import * as Expo from "expo";
+import ListScreen from "./ListScreen";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if(this.state.isReady === true) {
+      return (
+        <Container>
+          <Content>
+            <Button style={styles.spacing}>
+              <Text>Click Me!</Text>
+            </Button>
+          </Content>
+        </Container>
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <Spinner color='blue'></Spinner>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  spacing: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    alignSelf: "center",
+    marginTop: 200
+  }})
+
+const AppNavigator = createStackNavigator({
+    Home: App,
+    ListScreen: ListScreen
+    },
+    {
+      initialRouteName: "Home"
+    });
+
+export default createAppContainer(AppNavigator);
